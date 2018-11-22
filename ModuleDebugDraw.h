3991f0ae11dfa4dc9dee237c91926d0b2dcdb0da
lluissr <lluissr@gmail.com>
2018-11-22 20:45:49 +0100

    Add ModuleDebugDraw
    Added ModuleDebugDraw and debugdraw files for shading

---
+++ ModuleDebugDraw.h
@@ -0,0 +1,26 @@
+#ifndef _MODULE_DEBUGDRAW_H_
+#define _MODULE_DEBUGDRAW_H_
+
+#include "Module.h"
+
+class DDRenderInterfaceCoreGL;
+
+class ModuleDebugDraw : public Module
+{
+
+public:
+
+    ModuleDebugDraw();
+    ~ModuleDebugDraw();
+
+	bool            Init();
+	update_status   Update();
+	bool            CleanUp();
+
+    void            Draw(unsigned fbo, unsigned fb_width, unsigned fb_height);
+private:
+
+    static DDRenderInterfaceCoreGL* implementation;
+};
+
+#endif /* _MODULE_DEBUGDRAW_H_ */
